c0dc9b487837c490ce09040f2d039ac99d8d0810
shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2024-12-28 18:12:17 +0500

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import User from "./components/create_user";
+import Routers from "./routers";
+import { userStore } from "./zustand";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
-      <h2>Salom</h2>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Header />
+      <Routers />
+      <Outlet />
     </>
   );
 }
